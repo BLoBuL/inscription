d185d482886b00dc15fe22e4c24ce24cfd3e905a
RealET <real3t@gmail.com>
2025-05-16 17:54:21 +0200

    fix: il faut aussi pouvoir masquer le champ de reset de mot de passe

--- inscription3_pipelines.php
+++ inscription3_pipelines.php
@@ -1108,6 +1108,16 @@ function inscription3_editer_contenu_objet($flux) {
 			}
 		}
 
+		if (isset($config['password_reset'])
+			and $config['password_reset'] !== 'on') {
+				$flux['data'] = 'zzz'. preg_replace(
+					"/(<button [^>]*name=[\"']reset_password[\"'].*<\/button>)/Uims",
+					'',
+					$flux['data'],
+					1
+				);
+		}
+
 		/**
 		 * On vire le champs création du formulaire (ne doit pas être modifié manuellement)
 		 * Si on n'a pas ce champs rempli, on utilise la date actuelle pour le remplir
